a8f2d8a3586beb9ce74a53fb198a597fb6a0e49a
franasemb <francoaasemborn@gmail.com>
2023-05-14 02:01:38 -0300

    assets bug

--- src/app/components/modelos/modelos.component.ts
+++ src/app/components/modelos/modelos.component.ts
@@ -9,15 +9,15 @@ export class ModelosComponent {
   modelos = [
     {
       name: 'Pulpos',
-      url: 'assets/trabajos/pulpos/pulpo-azul-blanco-2.webp',
+      url: '/assets/trabajos/pulpos/pulpo-azul-blanco-2.webp',
     },
     {
       name: 'Osos',
-      url: 'assets/trabajos/osos/osa-violeta-2.webp',
+      url: '/assets/trabajos/osos/osa-violeta-2.webp',
     },
     {
       name: 'Conejos',
-      url: 'assets/trabajos/conejos/conejo-verde-3.webp',
+      url: '/assets/trabajos/conejos/conejo-verde-3.webp',
     },
   ];
 }
